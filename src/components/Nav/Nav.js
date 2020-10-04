17e09de969f9bbcb02eea8e0ab50062aa626771a
Hectordr03 <Hectordr03@gmail.com>
2020-10-04 12:15:47 -0400

    handle click

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,12 +2,16 @@ import React from 'react';
 import classes from './Nav.module.css';
 
 const Nav = () => {
+    const clicked = (e) => {
+        console.log(`${e.target.getAttribute("name")} button has been clicked!`)
+    }
+
     return (
         <div className={classes.Nav}>
             <ul className={classes.list}>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
+                <li name="home" onClick={clicked}>Home</li>
+                <li name="about" onClick={clicked}>About</li>
+                <li name="contact" onClick={clicked}>Contact</li>
             </ul>
         </div>
     );
